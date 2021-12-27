0fa858adc3d213dc6943912c7005bc8e8aba838a
KHALID <khalid.freelancerbd@gmail.com>
2021-12-28 00:32:00 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const nav_slider = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const menu_nav = document.querySelector('.menu-nav');
+    const menu_opn = document.querySelector('.menu-opn');
+    const menu_cls = document.querySelector('.menu-cls');
+
+    hamburger.addEventListener('click', () => {
+        menu_nav.classList.toggle('nav-active');
+        menu_opn.classList.toggle('menu-act');
+        menu_cls.classList.toggle('menu-non-act');
+    });
+}
+nav_slider();
